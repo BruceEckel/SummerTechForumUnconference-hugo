e844788f0ff678028c189ba3ff03af164a7aee88
Bruce Eckel <BruceTEckel@gmail.com>
2021-06-11 13:22:29 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# echo "Delete hugo-material-banner theme"
-rm -fr themes
-
-# echo "Download latest hugo-material-banner theme"
-# git clone https://github.com/MindviewLLC/hugo-material-banner.git themes/hugo-material-banner
-
 echo "Hugo version"
 hugo version
 
